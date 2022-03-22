5d57f13d121b091ae59b5bb71e16f0481671a3c4
omnia-web <omnia.webmail@gmail.com>
2022-03-22 21:28:48 +0100

    new font (poppins)

--- pages/_document.js
+++ pages/_document.js
@@ -18,6 +18,10 @@ class MyDocument extends Document {
                     <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
                     <link rel="shortcut icon" href="/icons/favicon.ico" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body className="bg-gray-50  text-gray-900 antialiased selection:bg-primary-200 dark:bg-dark-200 dark:text-white ">
                     <Main />
